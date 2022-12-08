a6b2271d4c00286639a4079238753a24450f957b
zsp255 <81824704+zsp255@users.noreply.github.com>
2022-12-08 15:49:12 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('localhost', 3737))
+server.listen()
+
+client_socket, client_address = server.accept()
+file = open('server_image.jpg', 'wb')
+
+str1 = "go"
+client_socket.send(str1.encode())
+
+image_chunk  = client_socket.recv(2048)
+
+while image_chunk:
+    file.write(image_chunk)
+    image_chunk  = client_socket.recv(2048)
+
+file.close()
+client_socket.close()
