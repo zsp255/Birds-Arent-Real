64cd721cd8313232869a510167d22ca0fd7fbd87
zsp255 <81824704+zsp255@users.noreply.github.com>
2022-12-08 17:02:38 -0600

    wifi change midway

--- gopro.py
+++ gopro.py
@@ -1,6 +1,9 @@
 from goprocam import GoProCamera
 from goprocam import constants
 from PIL import Image
+import os
+
+# Start with ZachsGoPro
 
 gopro1 = GoProCamera.GoPro(constants.gpcontrol)
 #gopro1.mode(constants.Mode.PhotoMode)
@@ -10,5 +13,16 @@ name1 = tmp.split("/")[5]
 name2 = tmp.split("/")[6]
 gopro1.downloadLastMedia()
 
-im = Image.open(name1+"-"+name2)
-im.show()
+#im = Image.open(name1+"-"+name2)
+#im.show()
+
+os.system("networksetup -setairportnetwork en0 NeelsSession goprosession")
+
+gopro2 = GoProCamera.GoPro(constants.gpcontrol) 
+
+tmp = gopro2.take_photo()
+name1 = tmp.split("/")[5]
+name2 = tmp.split("/")[6]
+gopro2.downloadLastMedia()
+
+
