60196bec8d1592eeeab3801219da610b7fbe5f8d
zsp255 <81824704+zsp255@users.noreply.github.com>
2022-12-09 12:21:25 -0600

    Create client2.py

---
+++ client2.py
@@ -0,0 +1,25 @@
+import socket
+from goprocam import GoProCamera
+from goprocam import constants
+import urllib.request
+import os
+
+gopro1 = GoProCamera.GoPro(constants.gpcontrol)
+tmp = gopro1.take_photo()
+urllib.request.urlretrieve(tmp, "testgp2.jpg")
+
+os.system("networksetup -setairportnetwork en0 AK hello123")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('172.20.10.8', 8080))
+
+file1 = open("testgp2.jpg", "rb")
+imagedata = file1.read(2048)
+
+with imagedata:
+    client.send(imagedata)
+    imagedata = file1.read(2048)
+
+
+file1.close()    
+client.close()
