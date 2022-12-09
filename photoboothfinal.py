b73276627a6dbbd1b14236016474fdb81e76e910
Angel Zou <angel@zenblen.com>
2022-12-09 14:21:58 -0600

    "final version!"

--- photoboothfinal.py
+++ photoboothfinal.py
@@ -1,12 +1,12 @@
 import cv2
 import os
 import time
-import numpy as np
+# import numpy as np
 from goprocam import GoProCamera
 from goprocam import constants
 import socket
 import urllib.request
-import PIL
+
 from PIL import Image, ImageOps, ImageDraw, ImageFont
 
 strip_rows = 4
@@ -14,35 +14,15 @@ strip_columns = 1
 strip_width = 300
 strip_height = 750
 
-column_grayscale = (0, 1)
 strip = Image.new('RGB', (strip_width, strip_height), (255,255,255))
 row_gutter = 15
-column_gutter = 10
-target_height = int((strip_height - ((strip_rows + 1) * row_gutter)) / strip_rows)
-target_width = int((strip_width-20 - ((strip_columns - 1) * column_gutter)) / strip_columns)
 
+target_height = int((strip_height - ((strip_rows + 1) * row_gutter)) / strip_rows)
+target_width = int(strip_width-20)
 
 
 gopro1 = GoProCamera.GoPro(constants.gpcontrol)
 gopro1.mode(constants.Mode.PhotoMode)
-
-### Helper functionss
-def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
-    dim = None
-    (h, w) = image.shape[:2]
-
-    if width is None and height is None:
-        return image
-    if width is None:
-        r = height / float(h)
-        dim = (int(w * r), height)
-    else:
-        r = width / float(w)
-        dim = (width, int(h * r))
-
-    return cv2.resize(image, dim, interpolation=inter)
-# Ref
-# https://stackoverflow.com/questions/35180764/opencv-python-image-too-big-to-display
 
 ### Style
 FONT = cv2.FONT_HERSHEY_DUPLEX
@@ -51,12 +31,10 @@ FONT = cv2.FONT_HERSHEY_DUPLEX
 ### Main Program
 cam = cv2.VideoCapture(0)
 
-# cv2.namedWindow("test")
 cv2.namedWindow("test_flipped")
 
 img_counter = 0
 time_stamp = None
-img_saved = False
 img_name = ""
 
 while True:
@@ -80,35 +58,13 @@ while True:
             cv2.putText(frame_flipped,'1',(500,300), FONT, 10,(233,255,255),5,cv2.LINE_AA)
             cv2.imshow("test_flipped", frame_flipped)   # 1
         else:
-            if img_saved:
-                # ### Load images & show images in multiple new small windows to populate the screen 
-                # my_img_1 = cv2.imread(img_name)
-                # my_img_1  = ResizeWithAspectRatio(my_img_1 , width=400) # Resize to window size
-                # window_name = "image_{}".format(img_counter)
-                # cv2.namedWindow(window_name)
-                # cv2.moveWindow(window_name, (img_counter%4)*350,(img_counter%4)*200)
-                # cv2.imshow(window_name, my_img_1)
-                # img_saved = False    
-
-                ### Basic photobooth function - show pic after end of count down
-                img_name = "opencv_frame_{}.jpg".format(img_counter)
-                cv2.imwrite(img_name, frame_no_text)
-                my_img_1  = ResizeWithAspectRatio(frame_no_text , width=400) # Resize to window size
-                window_name = "image_{}".format(img_counter)
-                cv2.namedWindow(window_name)
-                cv2.moveWindow(window_name, (img_counter%4)*350,(img_counter%4)*200)
-                cv2.imshow(window_name, my_img_1)
-
-                tmp = gopro1.take_photo()
-                print("gp photo taken")
-                urllib.request.urlretrieve(tmp, "testgp.jpg")
-                print("download")
-                im = Image.open("testgp.jpg")
-
-                img_saved = False
-                break
-            time_stamp = None
-            #break
+            ### Prompt the go pro to take picture
+            tmp = gopro1.take_photo()
+            print("gp photo taken")
+            urllib.request.urlretrieve(tmp, "testgp.jpg")
+            print("download")
+            break
+            # time stamp = None
 
     k = cv2.waitKey(1)
     if k%256 == 27:
@@ -122,21 +78,9 @@ while True:
         break
     elif k%256 == 32:   
         ### SPACE pressed --> birds start taking pic, assign time stamp & start count down
-
         if not time_stamp:
             ### Saving time stamp
             time_stamp = time.time()
-            ### Saving the image
-            # img_name = "opencv_frame_{}.png".format(img_counter)
-            # cv2.imwrite(img_name, frame_flipped)
-            # my_img_1 = np.zeros((512, 512, 1), dtype = "uint8")   # Black image placeholder
-
-            ### Save go pro pic
-            # pic1 = gopro1.downloadLastMedia(gopro1.take_photo())
-
-            img_saved = True
-            print("{} written!".format(img_name))
-            img_counter += 1
         else:   
             ### If time_stamp is on, refresh time_stamp
             time_stamp = time.time()
@@ -147,7 +91,6 @@ cv2.destroyAllWindows()
 
 print("destroy windows")
 t = time.time()
-
 
 print("switching network")
 #os.system("networksetup -setairportnetwork en0 C4\ Wifi Hoover2023")
@@ -174,30 +117,31 @@ while image_chunk:
 #     urllib.request.urlretrieve("http://10.150.66.178:8000/readme.md", "readme_download.md")
 
 file.close()
-client_socket.close() 
-    
+client_socket.close()    
 
 
 image = Image.open("opencv_frame_1.jpg")
+# image = Image.open("assets/slogan.jpeg")
 image = ImageOps.fit(image, (target_width, target_height))
 y = (0 * target_height) + ((0+1) * row_gutter)
 x = 10
 strip.paste(image, (x,y))
 
-#image = open("testgp.jpg")
 image = Image.open("testgp.jpg")
+# image = Image.open("assets/slogan.jpeg")
 image = ImageOps.fit(image, (target_width, target_height))
 y = (1 * target_height) + ((1+1) * row_gutter)
 x = 10
 strip.paste(image, (x,y))
 
 image = Image.open("server_image.jpg")
+# image = Image.open("assets/slogan.jpeg")
 image = ImageOps.fit(image, (target_width, target_height))
 y = (2 * target_height) + ((2+1) * row_gutter)
 x = 10
 strip.paste(image, (x,y))
 
-image = Image.open("assets/camera.jpeg")
+image = Image.open("assets/slogan.jpeg")
 image = ImageOps.fit(image, (target_width, target_height))
 y = (3 * target_height) + ((3+1) * row_gutter)
 x = 10
@@ -208,3 +152,5 @@ font = ImageFont.truetype("Chalkduster.ttf", 1)
 I1.text((238, 737), "12.9.2022", fill=(255, 0, 0))
 strip.save("photostrip.jpeg")
 strip.show()
+
+os.system("python3 tkinter_send_mail.py")
